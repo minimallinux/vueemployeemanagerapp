c6d97c7036e9905e23e0c0c75bec793ae4e91b09
minimallinux <anthonymcgowan1@gmail.com>
2018-06-05 13:09:09 +0100

    Use UIKit

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,11 @@
 <template>
 <div id="dashboard">
 <h2>Dashboard</h2>
+<div class="fixed-action-btn">
+<router-link to="/new" class="btn-floating btn-large red">
+<i class="fa fa-plus"></i>
+</router-link>
+</div>
 </div>
 </template>
 <script>
