a6794df1aadc3ec974e0a7597be2c7d0523ceed0
minimallinux <anthonymcgowan1@gmail.com>
2018-06-06 01:09:20 +0100

    Update

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,22 @@ color:aliceblue;
 .tm-container {
 padding-top: 30px;
 }
+.float{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:aqua;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+	box-shadow: 2px 2px 3px #999;
+}
 
+.btn-float{
+	margin-top:22px;
+}
 </style>
 
 
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,11 +1,11 @@
 <template>
 <div id="dashboard">
 <h3>Dashboard</h3>
-<button class="uk-button uk-button-default">
-<router-link to="/new">
-<i class="fa fa-plus"></i>
+
+<router-link to="/new" class="float">
+<i class="fa fa-plus fa-lg btn-float"></i>
 </router-link>
-</button>
+
 </div>
 </template>
 <script>
